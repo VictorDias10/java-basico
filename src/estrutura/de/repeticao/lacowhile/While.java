81dd50c3dd01fd6b2991e04c5d558eaea95e16f6
Victor <vdias201313@gmail.com>
2024-03-22 21:13:48 -0300

    Novo commit

--- src/estrutura/de/repeticao/lacowhile/While.java
+++ src/estrutura/de/repeticao/lacowhile/While.java
@@ -3,6 +3,6 @@ package estrutura.de.repeticao.lacowhile;
 public class While {
 	
 	public static void main(String[] args) {
-		System.out.println("Estrutura de repetição while");
+		System.out.println("Olá Mundo");
 	}
 }
